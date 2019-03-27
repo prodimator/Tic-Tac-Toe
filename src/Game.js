e4c569e7017ceea01887cf8dc7a46217f73ecdcc
Ryan Wolfe <rwolfe22@gmail.com>
2019-03-27 13:44:38 -0400

    start of logic

--- src/Game.js
+++ src/Game.js
@@ -9,7 +9,10 @@ class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      playerTurn: 'Player 1',
+      playerTurn: {
+        name: "Player 1",
+        value: 'x'
+      },
       playerOne: {
         name: '',
         score: '',
@@ -19,36 +22,68 @@ class Game extends Component {
         score: '',
       },
       board: {
-        row1: ['x', 'x', 'x'],
-        row2: ['o', 'o', 'o'],
-        row3: ['o', 'x', 'o']
+        row1: ['', '', ''],
+        row2: ['', '', ''],
+        row3: ['', '', '']
       }
     };
+    this.clickTile = this.clickTile.bind(this);
   }
+
+  clickTile(event) {
+    let index = event.currentTarget.id
+    if (this.state.board.row1[index] === ''){
+      let temp = this.state.board;
+      temp.row1[index] = this.state.playerTurn.value; 
+      this.setState({
+        board : temp
+      })
+    }
+  }
+
+  swapPlayer(){
+    if (this.state.playerTurn.name === "Player1"){
+      this.setState({
+        playerTurn: {
+          name: "Player 2",
+          value: "o"
+        }
+      })
+    }
+    if (this.state.playerTurn.name === "Player2"){
+      this.setState({
+        playerTurn: {
+          name: "Player 1",
+          value: "x"
+        }
+      })
+    }
+  }
+
   render() {
     return (
       <>
         <div className="Game">
-          <Header name={this.state.playerTurn} />
+          <Header name={this.state.playerTurn.name} />
         </div>
         <Row center="md">
           <Col md={6} sm={6} xs={6}>
             <table className="game-board">
               <tbody>
                 <tr id="row0">
-                  <td id="cell0-0"><Tile value={this.state.board.row1[0]} /></td>
-                  <td id="cell0-1"><Tile value={this.state.board.row1[1]} /></td>
-                  <td id="cell0-2"><Tile value={this.state.board.row1[2]} /></td>
+                  <td id="0" onClick={this.clickTile}><Tile value={this.state.board.row1[0]} /></td>
+                  <td id="1" onClick={this.clickTile}><Tile value={this.state.board.row1[1]} /></td>
+                  <td id="2" onClick={this.clickTile}><Tile value={this.state.board.row1[2]} /></td>
                 </tr>
                 <tr id="row1">
-                  <td id="cell1-0"><Tile value={this.state.board.row2[0]} /></td>
-                  <td id="cell1-1"><Tile value={this.state.board.row2[1]} /></td>
-                  <td id="cell1-2"><Tile value={this.state.board.row2[2]} /></td>
+                  <td id="0" onClick={this.clickTile}><Tile value={this.state.board.row2[0]} /></td>
+                  <td id="1" onClick={this.clickTile}><Tile value={this.state.board.row2[1]} /></td>
+                  <td id="2" onClick={this.clickTile}><Tile value={this.state.board.row2[2]} /></td>
                 </tr>
                 <tr id="row2">
-                  <td id="cell2-0"><Tile value={this.state.board.row3[0]} /></td>
-                  <td id="cell2-1"><Tile value={this.state.board.row3[1]} /></td>
-                  <td id="cell2-2"><Tile value={this.state.board.row3[2]} /></td>
+                  <td id="0" onClick={this.clickTile}><Tile value={this.state.board.row3[0]} /></td>
+                  <td id="1" onClick={this.clickTile}><Tile value={this.state.board.row3[1]} /></td>
+                  <td id="2" onClick={this.clickTile}><Tile value={this.state.board.row3[2]} /></td>
                 </tr>
               </tbody>
             </table>
